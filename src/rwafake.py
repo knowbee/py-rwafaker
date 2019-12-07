dc04a04497fc0b88ce5bf8a8dd2a24057e0147f1
Googler <knowbeeinc@gmail.com>
2019-12-07 19:47:35 +0200

    generate fake firstname

---
+++ src/rwafake.py
@@ -0,0 +1,16 @@
+from data import person
+from helpers import (randomizr, shuffler)
+
+class Faker:
+  def __init__(self):
+    self.person = person
+    self.patterns = [".", "_"]
+    self.n = None
+  def firstName(self, n=0):
+    try:
+      if(int(n) and int(n) < 101):
+        return shuffler(self.person["name"]["first_names"])[0:n]
+      return randomizr(self.person["name"]["first_names"])
+    except ValueError:
+      return "failed, wrong input"
+rwafaker = Faker()
