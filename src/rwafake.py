77ff4e1588c54135c8cb0471733dd7e5bffd62fc
Googler <knowbeeinc@gmail.com>
2019-12-07 19:48:23 +0200

    generate fake lastname

--- src/rwafake.py
+++ src/rwafake.py
@@ -13,4 +13,13 @@ class Faker:
       return randomizr(self.person["name"]["first_names"])
     except ValueError:
       return "failed, wrong input"
+  def lastName(self, n=0):
+    try:
+      if(int(n) and int(n) < 101):
+        return shuffler(self.person["name"]["last_names"])[0:n]
+      return randomizr(self.person["name"]["last_names"])
+    except ValueError:
+      return "failed, wrong input"
+
 rwafaker = Faker()
+
