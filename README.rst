de989b6e1e1899864d6b93b5fe88fc93e003f891
Googler <knowbeeinc@gmail.com>
2019-12-07 19:43:55 +0200

    initial commit

---
+++ README.rst
@@ -0,0 +1,33 @@
+rwa-faker
+===========
+
+This is a simple python package that generates massive amounts of realistic fake data in Rwanda native language (Ikinyarwanda)
+
+Installation
+------------
+
+The distribution is hosted on pypi at: https://pypi.python.org/pypi/rwa-faker/. To directly install the package from pypi, run from your terminal::
+
+    $ pip install rwa-faker
+
+Usage
+-----------
+
+```py
+from rwa-faker import rwafaker
+
+# Generate random first name
+fname = rwafaker.firstName(); // 'Bisamaza'
+
+# Generate random email
+email = rwafaker.email(); // 'bisamaza_trev@gmail.com'
+```
+Contributing
+------------
+
+Community contributions are welcome in the form of pull requests. When contributing code, please ensure:
+
+Authors
+-------
+
+`Igwaneza Bruce`_
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from os import path
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name="rwa-faker",
+  version='0.0.3',
+  description= "This package generates massive amounts of realistic fake data in Rwanda native language (Ikinyarwanda)",
+  long_description=long_description,
+  py_modules=["rwafake"],
+  package_dir={'': 'src'},
+  classifiers=[
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Development Status :: 4 - Beta",
+            "Environment :: Other Environment",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Education",
+            "License :: OSI Approved :: GNU General Public License (GPL)",
+            "Operating System :: OS Independent",
+            "Topic :: Software Development :: Libraries :: Python Modules"
+      ],
+      keywords= 'hi',
+      url='https://github.com/knowbee/py-rwa-faker',
+      author='Igwaneza Bruce',
+      author_email='knowbeeinc@gmail.com',
+      license='MIT',
+      zip_safe=False,
+
+)
