7ca4ba0538cc7536bfcb9a2147bc9a811bc4b1ed
knowbee <knowbeeinc@gmail.com>
2020-12-25 18:08:41 +0200

    version 2.0.6

--- setup.py
+++ setup.py
@@ -3,19 +3,21 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-  name="rwafaker",
-  version='2.0.5',
-  description="This package generates massive amounts of realistic fake data in Rwanda native language (Ikinyarwanda)",
-  long_description=long_description,
-  py_modules=["rwafake"],
-  package_dir={'': 'rwafake'},
-  classifiers=[
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 2.7",
+    name="rwafaker",
+    version='2.0.6',
+    url="https://github.com/knowbee/py-rwafaker.git",
+    description="This package generates massive amounts of realistic fake data in Rwanda native language (Ikinyarwanda)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["rwafake"],
+    package_dir={'': 'rwafake'},
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
@@ -26,11 +28,11 @@ setup(
             "License :: OSI Approved :: GNU General Public License (GPL)",
             "Operating System :: OS Independent",
             "Topic :: Software Development :: Libraries :: Python Modules"
-      ],
-      keywords= 'hi',
-      author='Igwaneza Bruce',
-      author_email='knowbeeinc@gmail.com',
-      license='MIT',
-      zip_safe=False,
+    ],
+    keywords='hi',
+    author='Igwaneza Bruce',
+    author_email='knowbeeinc@gmail.com',
+    license='MIT',
+    zip_safe=False,
 
 )
